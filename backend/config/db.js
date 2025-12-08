3fcab062803f2c260fe25b16ed63da5c363c9963
T-Narender <tnarendergoud6300@gmail.com>
2025-12-09 00:11:00 +0530

    deploying backend

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,16 +1,17 @@
 import mongoose from "mongoose";
 
 
-export const connectDB=async()=>{
+export const connectDB = async () => {
   try {
-    await mongoose.connect('mongodb+srv://tnarendergoud6300_db_user:Nari1236@cluster0.ulie1bm.mongodb.net/Resume')
-    .then(()=>{
+    const connection = process.env.MONGODB_URI;
+    await mongoose.connect(connection)
+      .then(() => {
         console.log('Connected to MongoDB');
-    })
+      })
   } catch (error) {
     console.error('Error connecting to MongoDB:', error);
   }
 };
 
 
-  
+
